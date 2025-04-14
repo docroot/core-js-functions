f30abcdf59e960a9b999591fdfe0028f12a47628
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2025-04-14 13:02:46 +0400

    feat: add solutions for last two tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -204,8 +204,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return (...restAgs) => fn(...args, ...restAgs);
 }
 
 /**
@@ -225,8 +225,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return () => {
+    const res = start;
+    start += 1;
+    return res;
+  };
 }
 
 module.exports = {
